9670f0f9a6661e58f583381f9bcedad4389e8687
derek <frankderekdick@gmail.com>
2019-04-29 20:31:43 +0800

    Initial commit.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,16 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="hello-world">
+    <div class="tab-container">
+      <div class="tabs">
+        <div class="tab" v-bind:class="{selected : activeTabIndex === 0}" v-on:click="activeTabIndex = 0">Tab 0</div>
+        <div class="tab" v-bind:class="{selected : activeTabIndex === 1}" v-on:click="activeTabIndex = 1">Tab 1</div>
+        <div class="tab" v-bind:class="{selected : activeTabIndex === 2}" v-on:click="activeTabIndex = 2">Tab 2</div>
+        <div class="tab" v-bind:class="{selected : activeTabIndex === 3}" v-on:click="activeTabIndex = 3">Tab 3</div>
+      </div>
+      <div class="underline-container">
+        <div class="tab__underline" v-bind:style="{marginLeft: 25 * activeTabIndex + '%'}"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,24 +19,73 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data: () => ({
+    activeTabIndex: 0
+  })
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .tab-container {
+    width: 40%;
+    margin: 0 auto;
+
+    display: -webkit-flex; /* For Safari */
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .tabs {
+    width: 100%;
+
+    display: -webkit-flex; /* For Safari */
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .tab {
+    color: #808080;
+    font-size: 1.2em;
+    padding: 0.4em;
+    margin: 0 0.4em;
+
+    cursor: pointer;
+
+    flex-grow: 1;
+
+    /* Make text content not selectable */
+    user-select: none;
+    -moz-user-select: none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    -o-user-select: none;
+  }
+
+  .tab.selected {
+    color: #000000;
+
+    transition: .3s;
+  }
+
+  .underline-container {
+    height: 0.25em;
+    width: 100%;
+  }
+
+  .tab__underline {
+    height: 0.25em;
+    width: 25%;
+    background-color: #000000;
+    border-radius: 0.125em;
+
+    transition: .3s;
+  }
 </style>
